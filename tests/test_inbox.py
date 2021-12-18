5d664c37cdebd080e84feaabd5fcb02b916ce420
maksfourlife <maksfourlife@gmail.com>
2021-12-18 19:53:44 +0300

    pytest cosmetics

--- tests/test_inbox.py
+++ tests/test_inbox.py
@@ -1,4 +1,5 @@
 from client.inbox import *
+import pytest
 
 
 account.address = "0xBdedBED81C6D7ceBDeDfFaecDeBd12063AF4f4fB"
@@ -8,13 +9,9 @@ def test_message_out_of_bounds_reverts():
     """Проверяет, что обращение к сообщению вне границ вызывывает исключение"""
 
     count = Inbox.count()
-    
-    try:
-        Inbox.load(count)
-    except KeyError:
-        return
 
-    assert False
+    with pytest.raises(KeyError):
+        Inbox.load(count)
 
 
 def test_message_in_bounds_returns():
